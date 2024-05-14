9b52933c919aec4ec2c6e511501cf2df04158d25
Muhammed Kara <n.muhammed.kara@gmail.com>
2024-05-14 16:32:16 +0200

    new lines ftw :)

--- middlewares/middleware.go
+++ middlewares/middleware.go
@@ -11,20 +11,26 @@ import (
 func AuthMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		requestTarget := r.Method + " " + strings.Split(r.URL.Path, "/")[1]
+
 		if !utils.ProtectedRoutes[requestTarget] {
 			next.ServeHTTP(w, r)
 			return
 		}
+
 		jwtToken, err := r.Cookie("jwt")
+
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusUnauthorized)
 			return
 		}
+
 		verifiedToken, err := utils.VerifyJwt(jwtToken.Value)
+
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusUnauthorized)
 			return
 		}
+
 		// extract user id from token for further usage
 		claims, _ := verifiedToken.Claims.(jwt.MapClaims)
 		userId := claims["userId"].(string)
