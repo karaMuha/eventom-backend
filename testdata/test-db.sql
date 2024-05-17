836dc04aff05cfeafe40e08e379537ef685b0b39
Muhammed Kara <n.muhammed.kara@gmail.com>
2024-05-17 15:51:06 +0200

    add test data script

---
+++ testdata/test-db.sql
@@ -0,0 +1,30 @@
+CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp" WITH SCHEMA pg_catalog;
+
+--users
+CREATE TABLE IF NOT EXISTS users (
+  id uuid PRIMARY KEY DEFAULT uuid_generate_v1mc(),
+  email TEXT NOT NULL UNIQUE,
+  password TEXT NOT NULL
+)
+
+--events
+CREATE TABLE IF NOT EXISTS events (
+  id uuid PRIMARY KEY DEFAULT uuid_generate_v1mc(),
+  event_name text NOT NULL,
+  event_description text NOT NULL,
+  event_location text NOT NULL,
+  event_date date NOT NULL,
+  user_id uuid NOT NULL,
+  FOREIGN KEY(user_id) REFERENCES users(id)
+);
+
+--registrations
+CREATE TABLE IF NOT EXISTS registrations (
+  id uuid PRIMARY KEY DEFAULT uuid_generate_v1mc(),
+  event_id uuid,
+  user_id uuid,
+  FOREIGN KEY(event_id) REFERENCES events(id),
+  FOREIGN KEY(user_id) REFERENCES users(id)
+)
+
