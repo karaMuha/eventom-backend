52223cac669b4af68a907c75b15352e8bbc8fd01
Muhammed Kara <n.muhammed.kara@gmail.com>
2024-05-22 17:34:57 +0200

    init commit

--- dbScripts/public_schema.sql
+++ dbScripts/public_schema.sql
@@ -1,6 +1,14 @@
 CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp" WITH SCHEMA pg_catalog;
 
+
+--users
+CREATE TABLE IF NOT EXISTS users (
+  id uuid PRIMARY KEY DEFAULT uuid_generate_v1mc(),
+  email TEXT NOT NULL UNIQUE,
+  password TEXT NOT NULL
+)
+
 --events
 CREATE TABLE IF NOT EXISTS events (
   id uuid PRIMARY KEY DEFAULT uuid_generate_v1mc(),
@@ -11,13 +19,6 @@ CREATE TABLE IF NOT EXISTS events (
   user_id uuid NOT NULL,
   FOREIGN KEY(user_id) REFERENCES users(id)
 );
-
---users
-CREATE TABLE IF NOT EXISTS users (
-  id uuid PRIMARY KEY DEFAULT uuid_generate_v1mc(),
-  email TEXT NOT NULL UNIQUE,
-  password TEXT NOT NULL
-)
 
 --registrations
 CREATE TABLE IF NOT EXISTS registrations (
